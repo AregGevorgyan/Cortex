e23bb3b7eb335269614a222dcaed478e9af8062c
Areg Gevorgyan <areggevorgyan5@gmail.com>
2025-09-30 17:46:07 -0400

    update optimizers

--- Basic/nn.py
+++ Basic/nn.py
@@ -146,16 +146,38 @@ class NeuralNetwork:
             pbar.set_postfix({"loss": float(loss)})
 
 class Optimizer:
-    def __init__(self, learning_rate, optimizer):
+    def __init__(self, learning_rate=0.01, optimizer="SGD", beta1=0.9, beta2=0.999, epsilon=1e-8):
         self.learning_rate = learning_rate
         self.optimizer = optimizer
-    
+        self.beta1 = beta1
+        self.beta2 = beta2
+        self.epsilon = epsilon
+        # Adam only
+        self.m = []
+        self.v = []
+        self.t = 0
+
     def SGD(self, params, grads):
         for param, grad in zip(params, grads):
             param -= self.learning_rate * grad
 
     def Adam(self, params, grads):
-        pass
+        if self.m is None:
+            self.m = [np.zeros_like(param) for param in params]
+            self.v = [np.zeros_like(param) for param in params]
+        
+        self.t += 1 
+        
+        for i, (param, grad) in enumerate(zip(params, grads)):
+            # update first momemnt estimate
+            self.m[i] = self.beta1 * self.m[i] + (1 - self.beta1) * grad
+            # update second moment estimate
+            self.v[i] = self.beta2 * self.v[i] + (1 - self.beta2) * (grad ** 2)
+            # bias correction
+            m_hat = self.m[i] / (1 - self.beta1 ** self.t)
+            v_hat = self.v[i] / (1 - self.beta2 ** self.t)
+            # update parameter
+            param -= self.learning_rate * m_hat / (np.sqrt(v_hat) + self.epsilon)
     
 class Utils:
     @staticmethod
